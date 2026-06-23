a011ef3337b0bc5ec76d50a8500496f4eaf1509e
Anas011232 <nulltohero@gmail.com>
2026-06-24 05:06:03 +0600

    feat: remove unused code and imports from Profile component

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,17 +1,3 @@
-// import { View, Text } from "react-native";
-// import { auth } from "../../src/services/firebase";
-
-// export default function Profile() {
-//   const user = auth.currentUser;
-
-//   return (
-//     <View style={{ padding:20 }}>
-//       <Text>{user?.displayName}</Text>
-//       <Text>{user?.email}</Text>
-//     </View>
-//   );
-// }
-
 import { useState } from "react";
 import {
   View,
